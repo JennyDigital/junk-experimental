dd01897a84d016fadb1aa3c224caf9a35850d5ae
JennyDigital <jennifer.a.gunn@outlook.com>
2025-11-09 15:53:22 +0000

    Initial commit

---
+++ CDC474CEU6_Tester/EWARM/CDC474CEU6_Tester CDC474CEU6_Tester/STM32_Startup.d
@@ -0,0 +1,3 @@
+CDC474CEU6_Tester\ CDC474CEU6_Tester/STM32_Startup.o: \
+ /home/jennifer/.rowley_associates_limited/CrossWorks\ for\ ARM/v5/packages/targets/STM32/STM32_Startup.s \
+ /home/jennifer/.rowley_associates_limited/CrossWorks\ for\ ARM/v5/packages/targets/STM32/STM32G474xx.vec
---
+++ CDC474CEU6_Tester/EWARM/CDC474CEU6_Tester CDC474CEU6_Tester/thumb_crt0.d
@@ -0,0 +1,2 @@
+CDC474CEU6_Tester\ CDC474CEU6_Tester/thumb_crt0.o: \
+ /usr/share/crossworks_for_arm_5.3/source/thumb_crt0.s
